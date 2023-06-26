58679f84f91c6219e27c6655780f4eaa000471f2
yllensc <yllensc18@gmail.com>
2023-06-26 15:26:30 -0500

    fix: :bug: Botón on/off
    Ajuste para encender y apagar correctamente la calculadora

--- main.js
+++ main.js
@@ -1,108 +1,3 @@
-//document.addEventListener('DOMContentLoaded', function() {
-//    const display = document.querySelector('.calculator__display');
-//    const keys = document.querySelector('.calculator__keys');
-//  
-//    let firstOperand = '';
-//    let operator = '';
-//    let secondOperand = '';
-//    let isCalculatorOn = false;
-//  
-//    keys.addEventListener('click', function(event) {
-//        if (event.target.matches('button')) {
-//          const key = event.target;
-//          const keyContent = key.textContent;
-//    
-//          if (keyContent === 'AC') {
-//            clearCalculator();
-//          } else if (keyContent === '=') {
-//            if (hasEnoughOperands()) {
-//              const result = calculateResult();
-//              display.textContent = result;
-//              setOperands(result, '', '');
-//            }
-//          } else if (isOperator(keyContent)) {
-//            if (canSetOperator()) {
-//              operator = keyContent;
-//              display.textContent += operator;
-//            }
-//          } else if (keyContent === 'on') {
-//            isCalculatorOn = true;
-//          } else if (keyContent === 'off') {
-//            isCalculatorOn = false;
-//          } else {
-//            appendNumber(keyContent);
-//          }
-//        }
-//      });
-//    
-//  
-//    function clearCalculator() {
-//      display.textContent = '0';
-//      setOperands('', '', '');
-//    }
-//  
-//    function hasEnoughOperands() {
-//      return firstOperand !== '' && operator !== '' && secondOperand !== '';
-//    }
-//  
-//    function calculateResult() {
-//      const num1 = parseFloat(firstOperand);
-//      const num2 = parseFloat(secondOperand);
-//  
-//      switch (operator) {
-//        case '+':
-//          return num1 + num2;
-//        case '-':
-//          return num1 - num2;
-//        case 'x':
-//          return num1 * num2;
-//        case '÷':
-//            if (num1/num2 == NaN){
-//                return "ERROR";
-//            }
-//          return num1 / num2;
-//        default:
-//          return '';
-//      }
-//    }
-//  
-//    function isOperator(keyContent) {
-//      return ['+', '-', 'x', '÷'].includes(keyContent);
-//    }
-//  
-//    function canSetOperator() {
-//      return firstOperand !== '' && operator === '' && secondOperand === '';
-//    }
-//  
-//    function appendNumber(keyContent) {
-//      if (operator === '') {
-//        firstOperand += keyContent;
-//      } else {
-//        secondOperand += keyContent;
-//      }
-//      display.textContent += keyContent;
-//    }
-//  
-//    function setOperands(first, op, second) {
-//      firstOperand = first;
-//      operator = op;
-//      secondOperand = second;
-//    }
-//  
-//    // Event listener para el botón "on"
-//    const onButton = document.querySelector('.key--on');
-//    onButton.addEventListener('click', function() {
-//      isCalculatorOn = true;
-//    });
-//  
-//    // Event listener para el botón "off"
-//    const offButton = document.querySelector('.key--off');
-//    offButton.addEventListener('click', function() {
-//      isCalculatorOn = false;
-//      clearCalculator();
-//    });
-//  });
-
 class Calculator {
   constructor() {
     this.displayValue = '0';
@@ -174,31 +69,34 @@ const clearButton = document.querySelector('[data-action="clear"]');
 const backspaceButton = document.querySelector('.key--backspace');
 // Obtén una referencia al botón de encendido/apagado
 const powerButton = document.querySelector('#power-button');
-
 // Obtén una lista de todos los botones de la calculadora
 const calculatorButtons = document.querySelectorAll('.calculator__keys button');
-
 // Define una función para bloquear o desbloquear los botones
 function toggleButtonsDisabled(disabled) {
+  if(!disabled){
   calculatorButtons.forEach((button) => {
-    button.disabled = disabled;
+    button.disabled = true;
+  });
+  powerButton.disabled = false;
+  displayElement.innerHTML = '';
+  calculator.clear();
+}
+else{
+  calculatorButtons.forEach((button) => {
+    button.disabled = false;
   });
 }
-
+}
 // Agrega un controlador de eventos al clic del botón de encendido/apagado
 powerButton.addEventListener('click', function () {
   const isCalculatorOn = this.classList.toggle('active');
-
+debugger
   // Bloquea o desbloquea los botones de la calculadora según el estado
-  toggleButtonsDisabled(!isCalculatorOn);
+  toggleButtonsDisabled(isCalculatorOn);
 });
 
 // Inicializa el estado de encendido/apagado al cargar la página
 powerButton.classList.add('active');
-toggleButtonsDisabled(false);
-
-
-
 // Escuchar evento de clic en el botón de borrado de caracteres
 backspaceButton.addEventListener('click', () => {
   calculator.getBackspaceClick();
